792e9ce4de280918b9a5c8a2beea79b06fa1f6fa
Joosep Pata <joosep.pata@gmail.com>
2023-01-24 18:54:01 +0200

    up

--- scripts/test_pipeline.sh
+++ scripts/test_pipeline.sh
@@ -3,4 +3,6 @@
 cd src
 
 wget https://jpata.web.cern.ch/jpata/mlpf/clic_edm4hep/reco_p8_ee_tt_ecm365_1.root
-python3 test.py reco_p8_ee_tt_ecm365_1.root
+
+#process EDM4HEP to training ntuple in .parquet format
+python3 edm4hep_to_ntuple.py reco_p8_ee_tt_ecm365_1.root reco_p8_ee_tt_ecm365_1.parquet
---
+++ src/edm4hep_to_ntuple.py
@@ -0,0 +1,17 @@
+import awkward as ak
+import numpy as np
+import sys
+import uproot
+
+if __name__ == "__main__":
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+
+    fi = uproot.open(infile)
+    events = fi["events"]
+
+    # ...
+
+    reco_jet_p4s = np.zeros((100000, 4), dtype=np.float32)
+
+    ak.to_parquet(ak.Record({"reco_jet_p4s": reco_jet_p4s}), outfile)
