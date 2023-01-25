93adaa54625f9c9c3d8f78c15f2b975e10f7b4d4
Joosep Pata <joosep.pata@cern.ch>
2023-01-25 14:00:21 +0200

    first attempt at jet clustering (#11)
    * first attempt at jet clustering
    
    * workaround

--- src/edm4hep_to_ntuple.py
+++ src/edm4hep_to_ntuple.py
@@ -1,7 +1,26 @@
 import awkward as ak
-import numpy as np
 import sys
 import uproot
+import fastjet
+import vector
+
+
+# Remap various PDG-IDs to just photon, electron, muon, tau, charged hadron, neutral hadron
+def map_pdgid_to_candid(pdgid, charge):
+    if pdgid == 0:
+        return 0
+
+    # photon, electron, muon
+    if pdgid in [22, 11, 13, 15]:
+        return pdgid
+
+    # charged hadron
+    if abs(charge) > 0:
+        return 211
+
+    # neutral hadron
+    return 130
+
 
 if __name__ == "__main__":
     infile = sys.argv[1]
@@ -9,10 +28,46 @@ if __name__ == "__main__":
     print("running:", sys.argv[0], infile, outfile)
 
     fi = uproot.open(infile)
-    events = fi["events"]
+    ev = fi["events"]
 
-    # ...
+    this_file_arrs = ev.arrays(["MCParticles", "MergedRecoParticles"])
 
-    reco_jet_p4s = np.zeros((100000, 4), dtype=np.float32)
+    idx0 = "RecoMCTruthLink#0/RecoMCTruthLink#0.index"
+    idx1 = "RecoMCTruthLink#1/RecoMCTruthLink#1.index"
 
-    ak.to_parquet(ak.Record({"reco_jet_p4s": reco_jet_p4s}), outfile)
+    idx_recoparticle = ev.arrays(idx0)[idx0]
+    idx_mcparticle = ev.arrays(idx1)[idx1]
+
+    # index in the MergedRecoParticles collection
+    this_file_arrs["idx_reco"] = idx_recoparticle
+
+    # index in the MCParticles collection
+    this_file_arrs["idx_mc"] = idx_mcparticle
+
+    arrs = this_file_arrs
+
+    # Prepare 4-momentum of reco particles
+    mrp = arrs["MergedRecoParticles"]
+    mrp = ak.Record({k.replace("MergedRecoParticles.", ""): mrp[k] for k in mrp.fields})
+    reco_p4 = vector.awk(
+        ak.zip({"mass": mrp["mass"], "x": mrp["momentum.x"], "y": mrp["momentum.y"], "z": mrp["momentum.z"]})
+    )
+
+    # Cluster AK4 jets from PF particles with min pt 2 GeV
+    jetdef = fastjet.JetDefinition(fastjet.antikt_algorithm, 0.4)
+    constituent_index = []
+
+    # workaround for https://github.com/scikit-hep/fastjet/issues/174
+    for iev in range(len(reco_p4.pt)):
+        cluster = fastjet.ClusterSequence(reco_p4[iev], jetdef)
+        ci = cluster.constituent_index(min_pt=2.0)
+        constituent_index.append(ci)
+    constituent_index = ak.from_iter(constituent_index)
+
+    p4_flat = reco_p4[ak.flatten(constituent_index, axis=-1)]
+    num_ptcls_per_jet = ak.num(constituent_index, axis=-1)
+    ret = ak.from_iter([ak.unflatten(p4_flat[i], num_ptcls_per_jet[i], axis=-1) for i in range(len(num_ptcls_per_jet))])
+    ret2 = vector.awk(ak.zip({"x": ret.x, "y": ret.y, "z": ret.z, "t": ret.tau}))
+    print(ret2)
+
+    ak.to_parquet(ak.Record({"reco_pf_p4s": ret2}), outfile)
