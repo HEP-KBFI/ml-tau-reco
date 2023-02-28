68f9595e9b8c060c9ce2cf039211f70a9b7e0fab
Laurits <laurits.tani@gmail.com>
2023-02-28 14:41:31 +0200

    add additional plots for weights, small typo fix; update paths

--- src/weight_tools.py
+++ src/weight_tools.py
@@ -39,7 +39,7 @@ def visualize_weights(weight_matrix, pt_bin_edges, eta_bin_edges, output_path):
     plt.close("all")
 
 
-def create_matrix(data, eta_bin_edges, pt_bin_edges):
+def create_matrix(data, y_bin_edges, x_bin_edges, y_property, x_property):
     p4s = vector.awk(
         ak.zip(
             {
@@ -50,11 +50,13 @@ def create_matrix(data, eta_bin_edges, pt_bin_edges):
             }
         )
     )
-
-    pt = p4s.pt.to_numpy()
-    eta = p4s.eta.to_numpy()
-    matrix = np.histogram2d(eta, pt, bins=(eta_bin_edges, pt_bin_edges))[0]
-    return matrix
+    x_property_ = getattr(p4s, x_property).to_numpy()
+    y_property_ = getattr(p4s, y_property).to_numpy()
+    if y_property == 'theta':
+        y_property_ = np.rad2deg(y_property_)
+    matrix = np.histogram2d(y_property_, x_property_, bins=(y_bin_edges, x_bin_edges))[0]
+    normalized_matrix = matrix/np.sum(matrix)
+    return normalized_matrix
 
 
 def get_weight_matrix(target_matrix, comp_matrix):
@@ -90,8 +92,8 @@ def process_single_file(input_path, weight_matrix, eta_bin_edges, pt_bin_edges):
     )
     eta_values = p4s.eta.to_numpy()
     pt_values = p4s.pt.to_numpy()
-    eta_bin = np.digitize(eta_values, bins=eta_bin_edges) - 1
-    pt_bin = np.digitize(pt_values, bins=pt_bin_edges) - 1
+    eta_bin = np.digitize(eta_values, bins=(eta_bin_edges[1:] + eta_bin_edges[:-1]) / 2) - 1
+    pt_bin = np.digitize(pt_values, bins=(pt_bin_edges[1:] + pt_bin_edges[:-1]) / 2) - 1
     matrix_loc = np.concatenate([eta_bin.reshape(-1, 1), pt_bin.reshape(-1, 1)], axis=1)
     weights = ak.from_iter([weight_matrix[tuple(loc)] for loc in matrix_loc])
     merged_info = {field: data[field] for field in data.fields}
@@ -106,17 +108,29 @@ def main(cfg: DictConfig):
     eta_bin_edges = np.linspace(
         cfg.weighting.variables.eta.range[0], cfg.weighting.variables.eta.range[1], cfg.weighting.variables.eta.n_bins
     )
+    theta_bin_edges = np.linspace(
+        cfg.weighting.variables.theta.range[0], cfg.weighting.variables.theta.range[1], cfg.weighting.variables.theta.n_bins
+    )
     pt_bin_edges = np.linspace(
         cfg.weighting.variables.pt.range[0], cfg.weighting.variables.pt.range[1], cfg.weighting.variables.pt.n_bins
     )
-    sig_matrix = create_matrix(sig_data, eta_bin_edges, pt_bin_edges)
-    bkg_matrix = create_matrix(bkg_data, eta_bin_edges, pt_bin_edges)
+    sig_matrix = create_matrix(sig_data, eta_bin_edges, pt_bin_edges, y_property="eta", x_property="pt")
+    bkg_matrix = create_matrix(bkg_data, eta_bin_edges, pt_bin_edges, y_property="eta", x_property="pt")
     sig_weights = get_weight_matrix(target_matrix=sig_matrix, comp_matrix=bkg_matrix)
-    bkg_weights = get_weight_matrix(target_matrix=bkg_matrix, comp_matrix=sig_weights)
+    bkg_weights = get_weight_matrix(target_matrix=bkg_matrix, comp_matrix=sig_matrix)
     sig_output_path = os.path.join(cfg.samples.ZH_Htautau.output_dir, "signal_weights.png")
     visualize_weights(sig_weights, pt_bin_edges, eta_bin_edges, sig_output_path)
     bkg_output_path = os.path.join(cfg.samples.QCD.output_dir, "bkg_weights.png")
     visualize_weights(bkg_weights, pt_bin_edges, eta_bin_edges, bkg_output_path)
+
+    sig_matrix_p_theta = create_matrix(sig_data, theta_bin_edges, pt_bin_edges, y_property="theta", x_property="p")
+    bkg_matrix_p_theta = create_matrix(bkg_data, theta_bin_edges, pt_bin_edges, y_property="theta", x_property="p")
+    sig_weights_p_theta = get_weight_matrix(target_matrix=sig_matrix_p_theta, comp_matrix=bkg_matrix_p_theta)
+    bkg_weights_p_theta = get_weight_matrix(target_matrix=bkg_matrix_p_theta, comp_matrix=sig_matrix_p_theta)
+    sig_output_path_p_theta = os.path.join(cfg.samples.ZH_Htautau.output_dir, "signal_weights_p_theta.png")
+    visualize_weights(sig_weights_p_theta, pt_bin_edges, theta_bin_edges, sig_output_path_p_theta)
+    bkg_output_path_p_theta = os.path.join(cfg.samples.QCD.output_dir, "bkg_weights_p_theta.png")
+    visualize_weights(bkg_weights_p_theta, pt_bin_edges, theta_bin_edges, bkg_output_path_p_theta)
     process_files(
         weight_matrix=sig_weights,
         eta_bin_edges=eta_bin_edges,
