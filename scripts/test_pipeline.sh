a33d985f5e2c37e924526d056f741aa435702ac8
Joosep Pata <joosep.pata@cern.ch>
2023-03-03 12:36:06 +0200

    New paths (#86)
    * tune lr for simple dnn
    
    * fix paths

--- scripts/test_pipeline.sh
+++ scripts/test_pipeline.sh
@@ -7,23 +7,23 @@ mkdir -p data
 cd ntuple
 
 #Download test files if they don't exist
-INFILE_TAU_DIR=/local/joosep/clic_edm4hep_2023_02_21/p8_ee_ZH_Htautau_ecm380/
+INFILE_TAU_DIR=/local/joosep/clic_edm4hep_2023_02_27/p8_ee_ZH_Htautau_ecm380
 if [ ! -d "$INFILE_TAU_DIR" ]; then
-    wget --directory-prefix ZH_Htautau -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/clic_edm4hep_2023_02_21/p8_ee_ZH_Htautau_ecm380/reco_p8_ee_ZH_Htautau_ecm380_200001.root
+    wget --directory-prefix ZH_Htautau -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/clic_edm4hep_2023_02_27/p8_ee_ZH_Htautau_ecm380/reco_p8_ee_ZH_Htautau_ecm380_200001.root
     INFILE_TAU_DIR=$PWD/ZH_Htautau
 else
     mkdir -p root_input/p8_ee_ZH_Htautau_ecm380
-    cp /local/joosep/clic_edm4hep_2023_02_21/p8_ee_ZH_Htautau_ecm380/reco_p8_ee_ZH_Htautau_ecm380_200001.root root_input/p8_ee_ZH_Htautau_ecm380/
+    cp /local/joosep/clic_edm4hep_2023_02_27/p8_ee_ZH_Htautau_ecm380/reco_p8_ee_ZH_Htautau_ecm380_200001.root root_input/p8_ee_ZH_Htautau_ecm380/
     INFILE_TAU_DIR=$PWD/root_input/p8_ee_ZH_Htautau_ecm380
 fi;
 
-INFILE_QCD_DIR=/local/joosep/clic_edm4hep_2023_02_21/p8_ee_qq_ecm380
+INFILE_QCD_DIR=/local/joosep/clic_edm4hep_2023_02_27/p8_ee_qq_ecm380
 if [ ! -d "$INFILE_QCD_DIR" ]; then
-    wget --directory-prefix QCD -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/clic_edm4hep_2023_02_21/p8_ee_qq_ecm380/reco_p8_ee_qq_ecm380_100001.root
+    wget --directory-prefix QCD -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/clic_edm4hep_2023_02_27/p8_ee_qq_ecm380/reco_p8_ee_qq_ecm380_100001.root
     INFILE_QCD_DIR=$PWD/QCD
 else
     mkdir -p root_input/p8_ee_qq_ecm380
-    cp /local/joosep/clic_edm4hep_2023_02_21/p8_ee_qq_ecm380/reco_p8_ee_qq_ecm380_100001.root root_input/p8_ee_qq_ecm380/
+    cp /local/joosep/clic_edm4hep_2023_02_27/p8_ee_qq_ecm380/reco_p8_ee_qq_ecm380_100001.root root_input/p8_ee_qq_ecm380/
     INFILE_QCD_DIR=$PWD/root_input/p8_ee_qq_ecm380
 fi;
 
