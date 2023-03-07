bf5cda836e38594f3d3fcd66566be2a541620977
saswatinandan <saswati.nandan@cern.ch>
2023-03-07 09:39:03 +0200

    add simple grid

--- src/build_grid.py
+++ src/build_grid.py
@@ -2,8 +2,14 @@ import awkward as ak
 import numpy as np
 import vector
 import json
+import os
+import time
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+from math import pi
 
-from part_var import part_var_list
+from part_var import Var
 from grid import CellIndex, CellGrid
 from basicTauBuilder import BasicTauBuilder
 
@@ -15,119 +21,143 @@ class GridBuilder(BasicTauBuilder):
         cfg = json.load(cfgFile)
         self._builderConfig = cfg["GridAlgo"]
         cfgFile.close()
-        self.parttype = ["ele", "gamma", "mu", "charged_cand", "neutral_cand"]
+        self.num_particles_in_grid = cfg["num_particles_in_grid"]
+        self.do_plot = cfg["plot"]
+        self.max_var = Var.max_value()
 
-    def build_p4(self, data, part_p4):
+    def build_p4(self, part=''):
+        if part:
+            return vector.awk(
+            ak.zip({"px": self.data[part].x, "py": self.data[part].y, "pz": self.data[part].z, "mass": self.data[part].tau})
+        )
         return vector.awk(
-            ak.zip({"px": data[part_p4].x, "py": data[part_p4].y, "pz": data[part_p4].z, "mass": data[part_p4].tau})
+            ak.zip({"px": self.data.event_reco_cand_p4s.x, "py": self.data.event_reco_cand_p4s.y, "pz": self.data.event_reco_cand_p4s.z, "mass": self.data.event_reco_cand_p4s.tau})
         )
 
-    def get_part_p4(self, parttype, data):
-        if parttype == "ele":
-            partp4 = data.event_reco_cand_p4s[data.event_reco_cand_pdg == 11]
-        elif parttype == "gamma":
-            partp4 = data.event_reco_cand_p4s[data.event_reco_cand_pdg == 21]
-        elif parttype == "mu":
-            partp4 = data.event_reco_cand_p4s[data.event_reco_cand_pdg == 13]
-        elif parttype == "charged_cand":
-            partp4 = data.event_reco_cand_p4s[data.event_reco_cand_pdg == 211]
-        elif parttype == "neutral_cand":
-            total_mask = ak.zeros_like(data.reco_cand_pdg)
-            for neutral_cand in [2112, 130]:
-                mask_addition = ak.where(
-                    data.reco_cand_pdg == neutral_cand, ak.ones_like(data.reco_cand_pdg), ak.zeros_like(data.reco_cand_pdg)
-                )
-                total_mask = total_mask + mask_addition
-            partp4 = data.event_reco_cand_p4s[total_mask > 0]
-        else:
-            assert 0
-        return vector.awk(ak.zip({"mass": partp4.tau, "px": partp4.x, "py": partp4.y, "pz": partp4.z}))
-
-    def signalCone(self, pt):
+    def signalCone(self):
         minpt = 30
         minr = 0.05
         cone = 3
-        return np.maximum(cone / np.maximum(pt, minpt), minr)
+        return np.maximum(cone / np.maximum(self.reco_tau_p4.pt, minpt), minr)
 
-    def process_onejet(self, etas, phis, parts_p4, part_type, grid):
-        part_var = part_var_list[part_type]
-        list_part_var = np.zeros((grid.nCellsEta * grid.nCellsPhi, len(part_var)))
-        for eta, phi, part_p4 in zip(etas, phis, parts_p4):
-            if eta is None or phi is None:  # it means eta, phi are not within inner or outer grid
-                continue
-            cellIndex = CellIndex(eta, phi)
-            flatcellIndex = grid.GetFlatIndex(cellIndex)
+    def process_onejet(self, etas, phis, inner):
+        list_part_var = np.zeros((self.max_var*self.num_particles_in_grid, self.grid.nCellsEta, self.grid.nCellsPhi))
+        if self.reco_tau_p4.pt[self.jetidx] == 0: return list_part_var
+        self.filledgrid=np.zeros((self.grid.nCellsEta, self.grid.nCellsPhi), dtype=int)
+        for idx, (eta, phi) in enumerate(zip(etas, phis)):
+            etaidx, phiidx = self.bin_idx_eta[self.jetidx][idx], self.bin_idx_phi[self.jetidx][idx]
+            self.images_count[self.jetidx][etaidx,phiidx] += 1
+            if (self.filledgrid[etaidx,phiidx]) >= 2: continue
+            filledpartidx = self.filledgrid[etaidx,phiidx]
+            offset = filledpartidx*self.max_var - 1
+            list_part_var[offset+Var.rel_pt.value][etaidx,phiidx] = (self.pt_sorted_cand_pt[self.pt_sorted_cone_mask>0][self.jetidx][idx]) / self.reco_tau_p4.pt[self.jetidx]
+            list_part_var[offset+Var.dtheta.value][etaidx,phiidx] = self.pt_sorted_cand_dtheta[self.pt_sorted_cone_mask>0][self.jetidx][idx]
+            list_part_var[offset+Var.dphi.value][etaidx,phiidx] = self.pt_sorted_cand_dphi[self.pt_sorted_cone_mask>0][self.jetidx][idx]
+            list_part_var[offset+Var.mass.value][etaidx,phiidx] = self.pt_sorted_cand_mass[self.pt_sorted_cone_mask>0][self.jetidx][idx]
+            list_part_var[offset+Var.pdgid.value][etaidx,phiidx] = self.pt_sorted_cand_pdgid[self.pt_sorted_cone_mask>0][self.jetidx][idx]
+            '''list_part_var[filledpartidx+5][etaidx,phiidx] = self.pt_sorted_cand_dxy[self.pt_sorted_cone_mask>0][self.jetidx][idx]
+            list_part_var[filledpartidx+6][etaidx,phiidx] = self.pt_sorted_cand_dz[self.pt_sorted_cone_mask>0][self.jetidx][idx]
+            list_part_var[filledpartidx+7][etaidx,phiidx] = self.pt_sorted_cand_d3[self.pt_sorted_cone_mask>0][self.jetidx][idx]
+            list_part_var[filledpartidx+8][etaidx,phiidx] = self.pt_sorted_cand_d0[self.pt_sorted_cone_mask>0][self.jetidx][idx]
+            list_part_var[filledpartidx+9][etaidx,phiidx] = self.pt_sorted_cand_z0[self.pt_sorted_cone_mask>0][self.jetidx][idx]
+            list_part_var[filledpartidx+10][etaidx,phiidx] = self.pt_sorted_cand_dz[self.pt_sorted_cone_mask>0][self.jetidx][idx]'''
+            self.filledgrid[etaidx,phiidx] += 1
+        return list_part_var
 
-            ##### fill electron variable ####
-            if part_type == "ele":
-                list_part_var[flatcellIndex][part_var["elept"]] += part_p4.pt
-                list_part_var[flatcellIndex][part_var["eleeta"]] += part_p4.eta
-                list_part_var[flatcellIndex][part_var["elephi"]] += part_p4.phi
-                list_part_var[flatcellIndex][part_var["elemass"]] += part_p4.mass
-            #### fill gamma variable #####
-            elif part_type == "gamma":
-                list_part_var[flatcellIndex][part_var["gammapt"]] += part_p4.pt
-                list_part_var[flatcellIndex][part_var["gammaeta"]] += part_p4.eta
-                list_part_var[flatcellIndex][part_var["gammaphi"]] += part_p4.phi
-                list_part_var[flatcellIndex][part_var["gammamass"]] += part_p4.mass
-            #### fill muon candidate #####
-            elif part_type == "mu":
-                list_part_var[flatcellIndex][part_var["mupt"]] += part_p4.pt
-                list_part_var[flatcellIndex][part_var["mueta"]] += part_p4.eta
-                list_part_var[flatcellIndex][part_var["muphi"]] += part_p4.phi
-                list_part_var[flatcellIndex][part_var["mumass"]] += part_p4.mass
-            #### fill charged candidate variable ####
-            elif part_type == "charge_candidate":
-                list_part_var[flatcellIndex][part_var["chargedpt"]] += part_p4.pt
-                list_part_var[flatcellIndex][part_var["chargedeta"]] += part_p4.eta
-                list_part_var[flatcellIndex][part_var["chargedphi"]] += part_p4.phi
-                list_part_var[flatcellIndex][part_var["chargedmass"]] += part_p4.mass
-            ### fill neutral candidate #####
-            elif part_type == "neutral_candidate":
-                list_part_var[flatcellIndex][part_var["neutralpt"]] += part_p4.pt
-                list_part_var[flatcellIndex][part_var["neutraleta"]] += part_p4.eta
-                list_part_var[flatcellIndex][part_var["neutralphi"]] += part_p4.phi
-                list_part_var[flatcellIndex][part_var["neutralmass"]] += part_p4.mass
-        return list_part_var.reshape(-1)
+    def calculate_dangle(self):
+        return np.sqrt(self.pt_sorted_cand_dtheta * self.pt_sorted_cand_dtheta + self.pt_sorted_cand_dphi * self.pt_sorted_cand_dphi)
+
+    def deltaphi(self, phi1, phi2):
+        diff = phi1-phi2
+        return np.arctan2(np.sin(diff), np.cos(diff))
+
+    def mask(self):
+        mask_greater = 2*pi - self.pt_sorted_cand_dphi
+        self.pt_sorted_cand_dphi = ak.where(self.pt_sorted_cand_dphi > pi, mask_greater, self.pt_sorted_cand_dphi)
+        mask_less = 2*pi + self.pt_sorted_cand_dphi
+        self.pt_sorted_cand_dphi = ak.where(self.pt_sorted_cand_dphi <= -pi, mask_less, self.pt_sorted_cand_dphi)
+
+    def pt_sorted_ftrs(self):
+        self.part_p4 = self.build_p4('event_reco_cand_p4s')
+        self.pt_sorted_idx = ak.argsort(self.part_p4.pt, ascending=False)
+        self.pt_sorted_cand_p4 = self.part_p4[self.pt_sorted_idx]
+        self.pt_sorted_cand_pt = self.pt_sorted_cand_p4.pt
+        self.pt_sorted_cand_theta = self.pt_sorted_cand_p4.theta
+        self.pt_sorted_cand_phi = self.pt_sorted_cand_p4.phi
+        self.pt_sorted_cand_mass = self.pt_sorted_cand_p4.mass
+        self.pt_sorted_cand_pdgid = self.data.event_reco_cand_pdg[self.pt_sorted_idx]
+        self.pt_sorted_cand_dtheta = self.reco_tau_p4.theta - self.pt_sorted_cand_theta
+        self.pt_sorted_cand_dphi = self.reco_tau_p4.phi - self.pt_sorted_cand_p4.phi
+        self.mask()
+        self.pt_sorted_cand_dangle = self.calculate_dangle()
+
+
+    def get_cone_mask(self, inner_grid):
+        if inner_grid:
+            return ak.where(self.pt_sorted_cand_dangle < self.signalcone, ak.ones_like(self.pt_sorted_cand_dangle), ak.zeros_like(self.pt_sorted_cand_dangle))
+        else:
+            mask_outside_signal = ak.where(self.pt_sorted_cand_dangle > self.signalcone, ak.ones_like(self.pt_sorted_cand_dangle), ak.zeros_like(self.pt_sorted_cand_dangle))
+            mask_inside_isolation = ak.where(self.pt_sorted_cand_dangle < 0.5, ak.ones_like(self.pt_sorted_cand_dangle), ak.zeros_like(self.pt_sorted_cand_dangle))
+            total_mask = mask_outside_signal + mask_inside_isolation
+            total_mask = ak.where(total_mask==2, ak.ones_like(self.pt_sorted_cand_dangle), ak.zeros_like(self.pt_sorted_cand_dangle))
+            return total_mask
+
+    def plot(self, cone):
+        plt.hist(ak.flatten(self.maskdphi), bins=self.bins, range=(-self.bin,self.bin))
+        plt.savefig(f'dphi_{cone}.png')
+        plt.close('all')
+        plt.hist(ak.flatten(self.maskdeta), bins=self.bins, range=(-self.bin,self.bin))
+        plt.savefig(f'dtheta_{cone}.png')
+        plt.close('all')
+        plt.imshow(np.average(self.images_count, axis=0));
+        plt.colorbar()
+        plt.savefig(f'test_{cone}.png')
+        plt.close('all')
 
     def processJets(self, data):
-        write_info = {}
-        reco_tau_p4 = self.build_p4(data, "tau_p4s")
-        signalcone = self.signalCone(reco_tau_p4.pt)
-        for part in self.parttype:
-            part_p4 = self.get_part_p4(part, data)
-            dr = reco_tau_p4.deltaR(part_p4)
-            deta = reco_tau_p4.eta - part_p4.eta
-            dphi = reco_tau_p4.phi - part_p4.phi
-
-            for cone in ["inner_grid", "outer_grid"]:
-                grid = CellGrid(
-                    self._builderConfig[cone]["n_cells"],
-                    self._builderConfig[cone]["n_cells"],
-                    self._builderConfig[cone]["cell_size"],
-                    self._builderConfig[cone]["cell_size"],
-                )
-                if cone == "inner_grid":
-                    part_inside_cone = ak.mask(part_p4, dr < signalcone)
-                    maskdeta = ak.mask(deta, dr < signalcone)
-                    maskdphi = ak.mask(dphi, dr < signalcone)
+        self.data = data
+        write_info = {field: data[field] for field in data.fields}
+        self.reco_tau_p4 = self.build_p4("tau_p4s")
+        self.signalcone = self.signalCone()
+        self.pt_sorted_ftrs()
+        for cone in ["inner_grid", "outer_grid"]:
+            self.grid = CellGrid(
+                self._builderConfig[cone]["n_cells"],
+                self._builderConfig[cone]["n_cells"],
+                self._builderConfig[cone]["cell_size"],
+                self._builderConfig[cone]["cell_size"],
+            )
+            self.bin = self.grid.MaxDeltaEta()
+            self.bins = self.grid.nCellsEta
+            self.bins_eta = np.linspace(-self.bin, self.bin, self.bins)
+            self.bins_phi = np.linspace(-self.bin, self.bin, self.bins)
+            self.images_count = np.zeros((len(self.reco_tau_p4), len(self.bins_eta)+1, len(self.bins_phi)+1))
+            self.pt_sorted_cone_mask = self.get_cone_mask(cone=="inner_grid")
+            self.maskdeta, self.maskdphi = self.pt_sorted_cand_dtheta[self.pt_sorted_cone_mask>0], self.pt_sorted_cand_dphi[self.pt_sorted_cone_mask>0]
+            self.bin_idx_eta = ak.unflatten(np.searchsorted(self.bins_eta, ak.flatten(self.maskdeta)), ak.count(self.maskdeta, axis=-1), axis=-1)
+            self.bin_idx_phi = ak.unflatten(np.searchsorted(self.bins_phi, ak.flatten(self.maskdphi)), ak.count(self.maskdphi, axis=-1), axis=-1)
+            for idx, eta in enumerate(self.bin_idx_eta): # this loops over all jet?
+                self.jetidx = idx
+                list_part_info_perjet = self.process_onejet(self.maskdeta[idx], self.maskdphi[idx], cone)
+                if idx == 0:
+                    list_part_info_alljet = list_part_info_perjet
                 else:
-                    part_inside_cone = ak.mask(part_p4, dr > signalcone)
-                    maskdeta = ak.mask(deta, dr > signalcone)
-                    maskdphi = ak.mask(dphi, dr > signalcone)
-                    part_inside_cone = ak.mask(part_inside_cone, dr < 0.5)
-                    maskdeta = ak.mask(maskdeta, dr < 0.5)
-                    maskdphi = ak.mask(maskdphi, dr < 0.5)
-                etacellindex, phicellindex = grid.getcellIndex(maskdeta, maskdphi)
-                for idx, eta in enumerate(etacellindex):  # this loops over all jet?
-                    list_part_info_perjet = self.process_onejet(eta, phicellindex[idx], part_inside_cone[idx], part, grid)
-                    if idx == 0:
-                        list_part_info_alljet = list_part_info_perjet
-                    else:
-                        list_part_info_alljet = np.concatenate((list_part_info_perjet, list_part_info_alljet))
-                list_ak = ak.from_numpy(list_part_info_alljet)
-                write_info.update({f"{cone}_{part}_block": list_ak})
-                assert list_part_info_alljet.shape[0] == pow(self._builderConfig[cone]["n_cells"], 2) * len(
-                    part_var_list[part]
-                ) * len(reco_tau_p4)
+                    list_part_info_alljet = np.concatenate((list_part_info_perjet, list_part_info_alljet))
+            list_ak = ak.from_numpy(list_part_info_alljet)
+            write_info.update({f"{cone}": list_ak})
+            if self.do_plot: self.plot(cone)
+        print('building grid is finished on # of jets: ', len(self.reco_tau_p4))
         return write_info
+
+if __name__ == "__main__":
+    grid = GridBuilder()
+    inputfile = '/local/laurits/CLIC_taus_20230215_v1/HPS/ZH_Htautau/reco_p8_ee_ZH_Htautau_ecm380_360.parquet'
+    data = ak.from_parquet(inputfile)
+    print('Time: ', time.strftime('%H:%M'))
+    data = grid.processJets(data)
+    outputdir = 'grid/ZH_Htautau'
+    if not os.path.exists(outputdir):
+        os.makedirs(outputdir)
+    print('Time: ', time.strftime('%H:%M'))
+    ak.to_parquet(ak.Record(data), os.path.join(outputdir, os.path.basename(inputfile)))
+    
--- src/part_var.py
+++ src/part_var.py
@@ -1,26 +1,13 @@
-import collections
+from enum import Enum, auto
+class Var(Enum):
+    rel_pt = auto()
+    dtheta = auto()
+    dphi = auto()
+    mass = auto()
 
-part_var_list = collections.OrderedDict(
-    [
-        (
-            "ele",
-            collections.OrderedDict([("elept", 0), ("eleeta", 1), ("elephi", 2), ("elemass", 3)]),
-        ),
-        (
-            "gamma",
-            collections.OrderedDict([("gammapt", 0), ("gammaeta", 1), ("gammaphi", 2), ("gammamass", 3)]),
-        ),
-        (
-            "mu",
-            collections.OrderedDict([("mupt", 0), ("mueta", 1), ("muphi", 2), ("mumass", 3)]),
-        ),
-        (
-            "charged_cand",
-            collections.OrderedDict([("chargedpt", 0), ("chargedeta", 1), ("chargedphi", 2), ("chargedmass", 3)]),
-        ),
-        (
-            "neutral_cand",
-            collections.OrderedDict([("neutralpt", 0), ("neutraleta", 1), ("neutralphi", 2), ("neutralmass", 3)]),
-        ),
-    ]
-)
+    ##### it should be last variable#####
+    pdgid = auto()
+
+    @classmethod
+    def max_value(cls):
+        return cls.pdgid.value
