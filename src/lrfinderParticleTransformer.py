corrected normalization of loss and accuracy (turns out that loss scales with batchsize^2 and not simply with batchsize !!)

src/lrfinderParticleTransformer.py
@@ -29,9 +29,15 @@ def get_split_files(cfg_filename, split):
 
 
 def train_one_epoch(idx_epoch, dataloader_train, dataloader_test, model, dev, loss_fn, use_per_jet_weights, optimizer):
+    print("<train_one_epoch>:")
     num_jets_train = len(dataloader_train.dataset)
+    print("#jets(train) = %i" % num_jets_train)
+    num_batches_train = len(dataloader_train)
+    print("#batches(train) = %i" % num_batches_train)
     loss_train = 0.0
+    loss_normalization_train = 0.0
     accuracy_train = 0.0
+    accuracy_normalization_train = 0.0
     model.train()
     for idx_batch, (X, y, weight) in enumerate(dataloader_train):
         # Compute prediction and loss
@@ -49,8 +55,11 @@ def train_one_epoch(idx_epoch, dataloader_train, dataloader_test, model, dev, lo
         else:
             loss = loss_fn(pred, y)
         loss_train += loss.sum().item()
+        loss_normalization_train += torch.flatten(loss).size(dim=0)
+        #print("batch #%i: loss = %1.3f" % (idx_batch, loss.mean().item()))        
         accuracy = (pred.argmax(dim=1) == y).type(torch.float32)
         accuracy_train += accuracy.sum().item()
+        accuracy_normalization_train += torch.flatten(accuracy).size(dim=0)
 
         # Backpropagation
         optimizer.zero_grad()
@@ -58,18 +67,22 @@ def train_one_epoch(idx_epoch, dataloader_train, dataloader_test, model, dev, lo
         optimizer.step()
 
         batchsize = pred.size(dim=0)
-        if (idx_batch % 100) == 0 or idx_batch >= (num_jets_train - batchsize):
-            running_loss = loss.mean().item()
-            num_jets_processed = min((idx_batch + 1) * batchsize, num_jets_train)
-            print(" Running loss: %1.6f  [%i/%s]" % (running_loss, num_jets_processed, num_jets_train))
+        num_jets_processed = min((idx_batch + 1) * batchsize, num_jets_train)
+        if (idx_batch % 100) == 0 or num_jets_processed >= (num_jets_train - batchsize):
+            print(" Running loss: %1.6f  [%i/%s]" % (loss.mean().item(), num_jets_processed, num_jets_train))
 
-    loss_train /= num_jets_train
-    accuracy_train /= num_jets_train
-    print("Train: Avg loss = %1.6f, accuracy = %1.2f" % (loss_train, 100 * accuracy_train))
+    loss_train /= loss_normalization_train
+    accuracy_train /= accuracy_normalization_train
+    print("Train: Avg loss = %1.6f, accuracy = %1.2f%%" % (loss_train, 100 * accuracy_train))
 
     num_jets_test = len(dataloader_test.dataset)
+    print("#jets(test) = %i" % num_jets_test)
+    num_batches_test = len(dataloader_test)
+    print("#batches(test) = %i" % num_batches_test)
     loss_test = 0.0
+    loss_normalization_test = 0.0
     accuracy_test = 0.0
+    accuracy_normalization_test = 0.0
     model.eval()
     with torch.no_grad():
         for idx_batch, (X, y, weight) in enumerate(dataloader_test):
@@ -84,14 +97,18 @@ def train_one_epoch(idx_epoch, dataloader_train, dataloader_test, model, dev, lo
                 loss = loss_fn(pred, y)
                 loss = loss * weight
             else:
-                loss = loss_fn(pred, y).item()
+                loss = loss_fn(pred, y)
             loss_test += loss.sum().item()
+            loss_normalization_test += torch.flatten(loss).size(dim=0)
             accuracy = (pred.argmax(dim=1) == y).type(torch.float32)
             accuracy_test += accuracy.sum().item()
+            accuracy_normalization_test += torch.flatten(accuracy).size(dim=0)
 
-    loss_test /= num_jets_test
-    accuracy_test /= num_jets_test
-    print("Test: Avg loss = %1.6f, accuracy = %1.2f" % (loss_test, 100 * accuracy_test))
+    loss_test /= loss_normalization_test
+    accuracy_test /= accuracy_normalization_test
+    print("Test: Avg loss = %1.6f, accuracy = %1.2f%%" % (loss_test, 100 * accuracy_test))
+
+    #raise ValueError("STOP.")
 
     return loss_train, loss_test
 
@@ -179,9 +196,12 @@ def lrfinderParticleTransformer(train_cfg: DictConfig) -> None:
 
         optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1.0e-2)
 
-        loss_train, loss_test = train_one_epoch(
-            0, dataloader_train, dataloader_test, model, dev, loss_fn, train_cfg.use_per_jet_weights, optimizer
-        )
+        loss_train, loss_test = None, None
+        # CV: use loss of SECOND training epoch as figure-of-merrit
+        for idx_epoch in range(2):
+            loss_train, loss_test = train_one_epoch(
+                idx_epoch, dataloader_train, dataloader_test, model, dev, loss_fn, train_cfg.use_per_jet_weights, optimizer
+            )
 
         points_lr.append(lr)
         points_loss_train.append(loss_train)
@@ -189,9 +209,10 @@ def lrfinderParticleTransformer(train_cfg: DictConfig) -> None:
 
         lr *= 1.2
 
-    (graph_train,) = plt.plot(points_lr, points_loss_train, "b", label="train")
-    (graph_test,) = plt.plot(points_lr, points_loss_test, "r", label="test")
+    ( graph_train, ) = plt.plot(points_lr, points_loss_train, "b", label="train")
+    ( graph_test, ) = plt.plot(points_lr, points_loss_test, "r", label="test")
     plt.xscale("log")
+    plt.yscale("log")
     plt.legend(handles=[graph_train, graph_test])
     plt.xlabel("lr")
     plt.ylabel("loss")