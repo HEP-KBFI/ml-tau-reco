fixed precision-recall curves

src/trainLorentzNet.py
@@ -79,7 +79,7 @@ def train_loop(
         accuracy_normalization_train += torch.flatten(accuracy).size(dim=0)
 
         class_true_train.extend(y.detach().cpu().numpy())
-        class_pred_train.extend(pred.argmax(dim=1).detach().cpu().numpy())
+        class_pred_train.extend(torch.softmax(pred, dim=1)[:, 1].squeeze().detach().cpu().numpy())
         false_positives_train += (
             ((y == torch.tensor(0)) * (pred.argmax(dim=1) == torch.tensor(1))).to(dtype=torch.float32).sum().item()
         )
@@ -104,6 +104,9 @@ def train_loop(
 
     tensorboard.add_scalar("Loss/train", loss_train, global_step=idx_epoch)
     tensorboard.add_scalar("Accuracy/train", 100 * accuracy_train, global_step=idx_epoch)
+    print("class_true_train = ", class_true_train)
+    print("class_pred_train = ", class_pred_train)
+    print("class_true:class_pred = ", [ "%1.0f:%1.4f" % (class_true_train[idx], class_pred_train[idx]) for idx in range(num_jets_train) ])
     tensorboard.add_pr_curve(
         "ROC_curve/train", np.array(class_true_train), np.array(class_pred_train), global_step=idx_epoch
     )
@@ -158,7 +161,7 @@ def test_loop(
             accuracy_normalization_test += torch.flatten(accuracy).size(dim=0)
 
             class_true_test.extend(y.detach().cpu().numpy())
-            class_pred_test.extend(pred.argmax(dim=1).detach().cpu().numpy())
+            class_pred_test.extend(torch.softmax(pred, dim=1)[:, 1].squeeze().detach().cpu().numpy())
             false_positives_test += (
                 ((y == torch.tensor(0)) * (pred.argmax(dim=1) == torch.tensor(1))).to(dtype=torch.float32).sum().item()
             )
@@ -358,7 +361,7 @@ def trainLorentzNet(train_cfg: DictConfig) -> None:
         print(" CPU-Util = %1.2f%%" % cpu_percent)
         print(" Memory-Usage = %i Mb" % (process.memory_info().rss / 1048576))
         if dev == "cuda":
-            print("GPU: N/A")
+            print("GPU:")
             run_command("nvidia-smi --id=%i" % torch.cuda.current_device())
         else:
             print("GPU: N/A")