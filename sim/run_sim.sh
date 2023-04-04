6dbc323332e41f09c44ed538565a3d7b9559d117
Joosep Pata <joosep.pata@gmail.com>
2023-04-04 11:29:05 +0300

    up

--- sim/run_sim.sh
+++ sim/run_sim.sh
@@ -9,13 +9,13 @@ set -x
 env
 df -h
 
-OUTDIR=/local/joosep/clic_edm4hep/
-SIMDIR=/home/joosep/ml-tau-reco/sim
 NEV=100
-
 NUM=$1 #random seed
 SAMPLE=$2 #main card
 
+#Change these as needed
+OUTDIR=/local/joosep/clic_edm4hep/
+SIMDIR=/home/joosep/ml-tau-reco/sim
 WORKDIR=/scratch/local/$USER/${SAMPLE}_${SLURM_JOB_ID}
 FULLOUTDIR=${OUTDIR}/${SAMPLE}
 
@@ -33,9 +33,13 @@ cp -R $SIMDIR/fcc/clicRec_e4h_input.py ./
 echo "Random:seed=${NUM}" >> card.cmd
 cat card.cmd
 
+#Use a tagged version of Key4HEP
 source /cvmfs/sw.hsf.org/spackages6/key4hep-stack/2023-01-15/x86_64-centos7-gcc11.2.0-opt/csapx/setup.sh
+
+#Run generation
 k4run $SIMDIR/fcc/pythia.py -n $NEV --Dumper.Filename out.hepmc --Pythia8.PythiaInterface.pythiacard card.cmd
 
+#Run simulation
 ddsim --compactFile $LCGEO/CLIC/compact/CLIC_o3_v14/CLIC_o3_v14.xml \
       --outputFile out_sim_edm4hep.root \
       --steeringFile clic_steer.py \
@@ -43,9 +47,11 @@ ddsim --compactFile $LCGEO/CLIC/compact/CLIC_o3_v14/CLIC_o3_v14.xml \
       --numberOfEvents $NEV \
       --random.seed $NUM
 
+#Run reconstruction
 k4run clicRec_e4h_input.py -n $NEV --EventDataSvc.input out_sim_edm4hep.root --PodioOutput.filename out_reco_edm4hep.root
-cp out_reco_edm4hep.root reco_${SAMPLE}_${NUM}.root
 
+#Copy the outputs
+cp out_reco_edm4hep.root reco_${SAMPLE}_${NUM}.root
 cp reco_${SAMPLE}_${NUM}.root $FULLOUTDIR/
 
 rm -Rf $WORKDIR
