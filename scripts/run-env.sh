5db30b9419d53cb48eb8ec4d1035823391c824b9
Laurits <laurits.tani@gmail.com>
2023-04-10 11:28:05 +0300

    update paths

--- scripts/run-env.sh
+++ scripts/run-env.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-singularity exec -B /local/ -B /scratch-persistent /home/software/singularity/pytorch.simg:2023-03-16 "$@"
+singularity exec -B /local/ -B /scratch/persistent /home/software/singularity/pytorch.simg:2023-03-16 "$@"
