5a96947ab6461b7c5b6df0e2a05094d106f266e7
saswatinandan <saswati.nandan@cern.ch>
2023-04-17 11:20:04 +0300

    remove pred_p4 and add jet_p4s in data

--- src/DeepTauBuilder.py
+++ src/DeepTauBuilder.py
@@ -20,28 +20,37 @@ class DeepTauBuilder(BasicTauBuilder):
     def processJets(self, data):
         ds = TauJetDatasetWithGrid()
         data_obj = Batch.from_data_list(ds.process_file_data(data), exclude_keys=["gen_tau_decaymode", "gen_tau_p4"])
-        pred_istau, pred_p4 = self.model(data_obj)
+        pred_istau = self.model(data_obj)
         pred_istau = list(torch.softmax(pred_istau, axis=-1)[:, 1].contiguous().detach().numpy())
         njets = len(data["tau_p4s"])
-        # pred_p4 = np.zeros((njets, 4))
-        pred_p4 = np.asfortranarray(pred_p4.contiguous().detach().numpy())
         # dummy placeholders for now
         tauCharges = np.zeros(njets)
         dmode = np.zeros(njets)
         # as a dummy placeholder, just return the first PFCand for each jet
         tau_cand_p4s = data["reco_cand_p4s"][:, 0:1]
-        tauP4 = vector.awk(
+        tau_p4s = vector.awk(
             ak.zip(
                 {
-                    "px": pred_p4[:,0],
-                    "py": pred_p4[:,1],
-                    "pz": pred_p4[:,2],
-                    "mass": pred_p4[:,3],
+                    "px": data["tau_p4s"].x,
+                    "py": data["tau_p4s"].y,
+                    "pz": data["tau_p4s"].z,
+                    "mass": data["tau_p4s"].tau
+                }
+            )
+        )
+        jet_p4s = vector.awk(
+            ak.zip(
+                {
+                    "px": data["reco_jet_p4s"].x,
+                    "py": data["reco_jet_p4s"].y,
+                    "pz": data["reco_jet_p4s"].z,
+                    "mass": data["reco_jet_p4s"].tau
                 }
             )
         )
         return {
-            "tau_p4s": tauP4,
+            "jet_p4s": jet_p4s,
+            "tau_p4s": tau_p4,
             "tauSigCand_p4s": tau_cand_p4s,
             "tauClassifier": pred_istau,
             "tau_charge": tauCharges,
