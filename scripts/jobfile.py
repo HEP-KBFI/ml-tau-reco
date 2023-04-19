631f6cf6c18bb15e3af1fb9d11e7f5bdd2bbee60
Christian Veelken <christian.veelken@cern.ch>
2023-04-19 11:42:38 +0300

    changed job submission script such that it works for different users

--- scripts/jobfile.py
+++ scripts/jobfile.py
@@ -24,11 +24,11 @@ user = getpass.getuser()
 
 
 def create_batchfile(cmd, idx):
-    output_dir = os.path.join("/home/snandan", algo)
+    output_dir = os.path.join("/home/", user, algo)
     os.makedirs(output_dir, exist_ok=True)
     os.makedirs(os.path.join(output_dir, "error"), exist_ok=True)
     os.makedirs(os.path.join(output_dir, "out"), exist_ok=True)
-    scratch_dir = os.path.join("/scratch", "persistent", "snandan", algo, sample)
+    scratch_dir = os.path.join("/scratch", "persistent", user, algo, sample)
     os.makedirs(scratch_dir, exist_ok=True)
     job_file = os.path.join(scratch_dir, str(idx) + ".sh")
     error_file = os.path.join(output_dir, "error", str(idx))
@@ -58,8 +58,8 @@ def create_batchfile(cmd, idx):
 
 files = glob.glob(f"{inputpath}/*")
 for idx, f in enumerate(range(0, len(files) + 1, nfiles)):
-    cmd = f"/home/{user}/mltaureco/ml-tau-reco/scripts/run-env.sh\
-    python3 /home/{user}/mltaureco/ml-tau-reco/src/runBuilder.py builder={algo}\
+    cmd = f"/home/{user}/ml-tau-reco/scripts/run-env.sh\
+    python3 /home/{user}/ml-tau-reco/src/runBuilder.py builder={algo}\
     samples_to_process=['{sample}'] n_files={nfiles}\
     start={f} output_dir=dir_$SLURM_JOBID verbosity=1\
     samples.{sample}.output_dir={inputpath} use_multiprocessing=False"
