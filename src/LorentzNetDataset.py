b45663a242cc32f67f32853908e42b65603ec758
Christian Veelken <christian.veelken@cern.ch>
2023-05-08 14:30:51 +0300

    improved code formatting

--- src/LorentzNetDataset.py
+++ src/LorentzNetDataset.py
@@ -119,7 +119,7 @@ class LorentzNetDataset(Dataset):
             # CV: pdgId=111 added to work around the bug fixed in this commit:
             #       https://github.com/HEP-KBFI/ml-tau-reco/pull/135/files#diff-9b848ad8e5903b4346d4030ebe41a391612220637cdd302d30d34b3fa07c96ea
             #    (this work-around allows us to keep using old files)
-            self.pdgId_embedding = OneHotEncoder(handle_unknown="ignore", sparse_output=False).fit(               
+            self.pdgId_embedding = OneHotEncoder(handle_unknown="ignore", sparse_output=False).fit(
                 [[11], [13], [22], [111], [130], [211], [2212]]
             )
 
